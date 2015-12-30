5d06c4001ddc588fb6fb14494372eff9bb0c8b47
Rostislav Novak <rostislav@ordinarynick.com>
2015-12-30 20:46:48 +0100

    Added some string append and so on. But not function.

--- test/resource/java/Test.java
+++ test/resource/java/Test.java
@@ -3,19 +3,19 @@ public class Test {
     public native void Print(String text);
 
     public void hello() {
-        Print("Hello from program.");
+        //Print("Hello from program.");
         int a = 3;
         Print("a is " + a);
-        int b = 2;
+        /*int b = 2;
         Print("b is " + b);
         int c = a + b;
         Print("a + b = " + c );
 
-        if(a > b) {
+        /*if(a > b) {
             Print("a is greater than b");
         } else {
             Print("b is greater then a");
-        }
+        }*/
     }
 
 
