a39bd76a9c3b445f02b8244fe96df064115186e3
Rostislav Novak <rostislav@ordinarynick.com>
2015-12-30 14:56:01 +0100

    Repaired bug in method invoke and native method invoke. Still remain bug in native method.

--- lib/mrjvm/native/runtime_environment.rb
+++ lib/mrjvm/native/runtime_environment.rb
@@ -1,18 +1,18 @@
 
 module Native
   class RuntimeEnvironment
-    attr_accessor :frame_stack, :class_heap, :object_heap, :critical_section
+    attr_accessor :frame_stack, :class_heap, :object_heap, :critical_section, :fp
 
     def run(native)
       self.method(native.to_sym).call(self)
     end
 
-
     def native_print(rte)
-      frame = rte.frame_stack[0]
-      object = frame.stack[frame.sp]
-      variable = rte.object_heap.get_object(object)
-      puts variable.variables[1]
+      frame = rte.frame_stack[fp]
+      puts frame.stack[frame.sp-1]
+      0
+      # variable = rte.object_heap.get_object(object_id)
+      #puts variable.variables[1]
     end
   end
 
