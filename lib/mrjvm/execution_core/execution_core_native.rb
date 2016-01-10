a3c38e04212c9652642923f2ac2eb144b2c8bcce
Rostislav Novak <rostislav@ordinarynick.com>
2016-01-10 12:21:23 +0100

    Execution core separeted to modules.

---
+++ lib/mrjvm/execution_core/execution_core_native.rb
@@ -0,0 +1,59 @@
+module ExecutionCoreNative
+  def execute_native_method(frame_stack)
+    MRjvm.debug('Invoking native method.')
+
+    frame = frame_stack[@fp]
+    java_class = frame.java_class
+    class_name = java_class.this_class_str
+    method_name = java_class.get_from_constant_pool(frame.method[:name_index])
+    method_descriptor = java_class.get_from_constant_pool(frame.method[:descriptor_index])
+
+    MRjvm.debug('Invoking native method: ' << method_name << ', descriptor: ' << method_descriptor)
+
+    signature = class_name + '@' + method_name + method_descriptor
+    native_method = get_fake_native_method(signature)
+
+    runtime_environment = Native::NativeRunner.new
+    runtime_environment.frame = frame
+    runtime_environment.class_heap = class_heap
+    runtime_environment.object_heap = object_heap
+    if native_method.include? 'true_native'
+      runtime_environment.run(signature, true)
+    else
+      runtime_environment.run(native_method, false)
+    end
+  end
+
+  # TODO: Only for testing
+  def get_fake_native_method(signature)
+    if signature.include? 'java/lang/String@valueOf(F)Ljava/lang/String;'
+      'string_value_of_f'
+    elsif signature.include? 'java/lang/String@valueOf(J)Ljava/lang/String;'
+      'string_value_of_j'
+    elsif signature.include? 'java/lang/StringBuilder@append(Ljava/lang/String;)Ljava/lang/StringBuilder;'
+      'string_builder_append_s'
+    elsif signature.include? 'java/lang/StringBuilder@append(I)Ljava/lang/StringBuilder;'
+      'string_builder_append_number'
+    elsif signature.include? 'java/lang/StringBuilder@append(C)Ljava/lang/StringBuilder;'
+      'string_builder_append_c'
+    elsif signature.include? 'java/lang/StringBuilder@append(Z)Ljava/lang/StringBuilder;'
+      'string_builder_append_z'
+    elsif signature.include? 'java/lang/StringBuilder@append(Ljava/lang/Object;)Ljava/lang/StringBuilder;'
+      'string_builder_append_o'
+    elsif signature.include? 'java/lang/StringBuilder@append(F)Ljava/lang/StringBuilder;'
+      'string_builder_append_number'
+    elsif signature.include? 'java/lang/StringBuilder@append(J)Ljava/lang/StringBuilder;'
+      'string_builder_append_number'
+    elsif signature.include? 'java/lang/StringBuilder@append(D)Ljava/lang/StringBuilder;'
+      'string_builder_append_number'
+    elsif signature.include? 'java/lang/StringBuilder@toString()Ljava/lang/String;'
+      'string_builder_to_string_string'
+    elsif signature.include? 'java/io/PrintStream@println(Ljava/lang/String;)V'
+      'native_print'
+    elsif signature.include? 'java/lang/System@loadLibrary(Ljava/lang/String;)V'
+      'load_native_library'
+    else
+      'true_native'
+    end
+  end
+end
